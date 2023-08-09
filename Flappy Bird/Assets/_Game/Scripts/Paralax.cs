51a1d8f2001c0979d8cddf00a85d7ef59b3df963
Joallisson <joallissonnc@gmail.com>
2023-08-09 20:04:47 -0300

    Criando animção do personagem, criado movimentaão do background e ground

---
+++ Flappy Bird/Assets/_Game/Scripts/Paralax.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    [SerializeField] float speedTexture;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+    }
+
+
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(speedTexture * Time.deltaTime, 0);
+    }
+}

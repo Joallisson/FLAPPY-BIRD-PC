5b790ea3ffc10fd88d8169f6908249c29f7cf66e
Joallisson <joallissonnc@gmail.com>
2023-08-10 21:30:00 -0300

    Criado spawn do canos aleatoriamente

--- Flappy Bird/Assets/_Game/Scripts/Pipe.cs
+++ Flappy Bird/Assets/_Game/Scripts/Pipe.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour
 {
-    [SerializeField] float speed = -2f;
-    
+    [SerializeField] float speed, leftBorder;
+
     void Start()
     {
-        
+        speed = -2f;
+        leftBorder = Camera.main.ScreenToViewportPoint(Vector3.zero).x - 10;
     }
 
     
     void Update()
     {
         transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x <= leftBorder)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Flappy Bird/Assets/_Game/Scripts/Spawner.cs
+++ Flappy Bird/Assets/_Game/Scripts/Spawner.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject pipePrefab;
-    [SerializeField] float time = 2f;
+    [SerializeField] float time, minY, maxY;
 
     void Start()
     {
         InvokeRepeating(nameof(Spawn), time, time);
+        time = 2f;
+        minY = -2f;
+        maxY = 2f;
     }
 
 
@@ -21,6 +24,6 @@ public class Spawner : MonoBehaviour
     private void Spawn()
     {
         GameObject newPipe = Instantiate(pipePrefab, transform.position, Quaternion.identity);
-        newPipe.transform.position += new Vector3();
+        newPipe.transform.position += new Vector3(0, Random.Range(minY, maxY));
     }
 }
